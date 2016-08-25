ce5705884afbf870854ab17b5129a5570e9b0a09
Liwsh <lwsh.95@foxmail.com>
2016-08-25 16:04:57 +0800

    Update servo.c

--- servo.c
+++ servo.c
@@ -1,15 +1,15 @@
 /*
  * serv.c
  *
- *  Created on: 2016Äê7ÔÂ27ÈÕ
- *      Author: ÀîÎÄÊ¤
+ *  Created on: 2016å¹´7æœˆ27æ—¥
+ *      Author: ææ–‡èƒœ
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
 #include <mraa.h>
-#include <servo.h>
+#include "servo.h"
 mraa_result_t haltPwm() {
 	return mraa_pwm_enable(m_pwmServoContext, 0);
 }
@@ -29,14 +29,21 @@ int calcPulseTraveling(int value) {
 			+ ((float) value / m_maxAngle)
 					* ((float) m_maxPulseWidth - (float) m_minPulseWidth));
 }
-int setAngle(int angle) {
+int setAngle(int angle, int pin) {
 	if (angle > m_maxAngle || angle < 0) {
 		return MRAA_ERROR_UNSPECIFIED;
 	}
 
-	mraa_pwm_enable(m_pwmServoContext, 1);
-	mraa_pwm_period_us(m_pwmServoContext, m_period);
-	mraa_pwm_pulsewidth_us(m_pwmServoContext, calcPulseTraveling(angle));
+	if (pin == pin3) {
+		mraa_pwm_enable(m_pwmServoContext, 1);
+		mraa_pwm_period_us(m_pwmServoContext, m_period);
+		mraa_pwm_pulsewidth_us(m_pwmServoContext, calcPulseTraveling(angle));
+	}
+	if (pin == pin9) {
+		mraa_pwm_enable(m_pwmServoContext1, 1);
+		mraa_pwm_period_us(m_pwmServoContext1, m_period);
+		mraa_pwm_pulsewidth_us(m_pwmServoContext1, calcPulseTraveling(angle));
+	}
 
 	if (m_waitAndDisablePwm) {
 		sleep(1); // we must make sure that we don't turn off PWM before the servo is done moving.
@@ -46,7 +53,8 @@ int setAngle(int angle) {
 	m_currAngle = angle;
 	return MRAA_SUCCESS;
 }
-void init(int pin, int minPulseWidth, int maxPulseWidth, int waitAndDisablePwm) {
+void init(int pin1, int pin2, int minPulseWidth, int maxPulseWidth,
+		int waitAndDisablePwm) {
 	m_minPulseWidth = minPulseWidth;
 	m_maxPulseWidth = maxPulseWidth;
 	m_period = PERIOD;
@@ -54,41 +62,20 @@ void init(int pin, int minPulseWidth, int maxPulseWidth, int waitAndDisablePwm) 
 	m_waitAndDisablePwm = waitAndDisablePwm;
 
 	m_maxAngle = 180.0;
-	m_servoPin = pin;
 
-	if (!(m_pwmServoContext = mraa_pwm_init(m_servoPin))) {
-		printf("mraa_pwm_init() failed, invalid pin?");
-		return;
-	}
+	m_pwmServoContext = mraa_pwm_init(pin1);
+	m_pwmServoContext1 = mraa_pwm_init(pin2);
 
 	m_currAngle = 180;
 
-	setAngle(0);
+//	setAngle(0, pin3);
+//	setAngle(0, pin9);
 }
-int main() {
-	init(9, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, DEFAULT_WAIT_DISABLE_PWM);
-	while (1) {
-		while (1) {
-			setAngle(180);
-			printf("Set angle to 180");
-			sleep(1);
-			setAngle(90);
-			printf("Set angle to 90");
-			sleep(1);
-			setAngle(0);
-			printf("Set angle to 0");
-			sleep(1);
-			setAngle(90);
-			printf("Set angle to 90");
-			sleep(1);
-			setAngle(180);
-			printf("Set angle to 180");
-			//! [Interesting]
-
-			printf("exiting application");
-		}
-
-		return 0;
-	}
+void servo(int angle,int pin){
+	pin3 = 3;
+	pin9 = 9;
+	init(pin3, pin9, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH,
+			DEFAULT_WAIT_DISABLE_PWM);
+			setAngle(angle,pin);
 }
 
